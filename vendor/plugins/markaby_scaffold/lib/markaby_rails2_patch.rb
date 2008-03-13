60a98876060cf68e195753bfe16da2ee05d73e9a
Alon Salant <alon@salant.org>
2008-03-13 00:26:15 +0000

    Support line numbers in markaby errors.

--- vendor/plugins/markaby_scaffold/lib/markaby_rails2_patch.rb
+++ vendor/plugins/markaby_scaffold/lib/markaby_rails2_patch.rb
@@ -1,6 +1,7 @@
-# This monkey patch fixes an exception specific to Rails 2.0.2 by replacing the definition of
-# ActionView::Base#render_template that Markaby overrides in markaby/rails.rb with the original
-# definition from Rails at http://svn.rubyonrails.org/rails/tags/rel_2-0-2/actionpack/lib/action_view/base.rb
+# Fix up Markaby to address an exception specific to Rails 2.0.2.
+# Replace the definition of ActionView::Base#render_template that Markaby overrides in markaby/rails.rb with the original
+# definition from Rails at http://svn.rubyonrails.org/rails/tags/rel_2-0-2/actionpack/lib/action_view/base.rb.
+# Then handle Markaby templates specifically to get line numbers in errors.
 module ActionView # :nodoc:
   class Base # :nodoc:
     # Renders the +template+ which is given as a string as either erb or builder depending on <tt>template_extension</tt>.
@@ -15,21 +16,18 @@ module ActionView # :nodoc:
         delegate_render(handler, template, local_assigns)
       end
     end
-  end
-end
 
-# Changes Markaby::Rails::ActionViewTemplateHandler#render to make file_path optional
-module Markaby
-  module Rails
-    class ActionViewTemplateHandler # :nodoc:
-      def initialize(action_view)
-        @action_view = action_view
-      end
-      def render(template, local_assigns, file_path=nil)
-        template = Template.new(template)
-        template.path = file_path
-        template.render(@action_view.assigns.merge(local_assigns), @action_view)
+   def render_template_with_markaby_line_support(template_extension, template, file_path = nil, local_assigns = {})
+     handler = self.class.handler_for_extension(template_extension)
+
+     if (handler == Markaby::Rails::ActionViewTemplateHandler)
+        template ||= read_template_file(file_path, template_extension)
+        handler.new(self).render(template, local_assigns, file_path)
+      else
+       render_template_without_markaby_line_support(template_extension, template, file_path, local_assigns)
       end
     end
+
+   alias_method_chain :render_template, :markaby_line_support
   end
 end
