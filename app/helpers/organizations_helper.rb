44ae7d4abf0eecf4bff7241f76ab1269f9c41281
Alon Salant <alon@salant.org>
2008-03-23 04:40:02 +0000

    Fix warnings.

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -2,11 +2,11 @@ module OrganizationsHelper
   def organization_form_fields(form)
     markaby do
       labeled_input 'Name', :for => :name do
-        text form.text_field :name
+        text form.text_field(:name)
         span.instructions "e.g. San Francisco Bike Kitchen"
       end
       labeled_input 'Key', :for => :key do
-        text form.text_field :key
+        text form.text_field(:key)
         span.instructions "e.g. sfbk, used for URL http://freehub.bikekitchen.org/sfbk"
       end
       labeled_input 'Timezone', :for => :timezone do
