627305116191167d0d214aac49edda4ba0add8c4
Alon Salant <alon@salant.org>
2008-03-23 03:47:56 +0000

    Content, form instructions.

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -2,10 +2,12 @@ module OrganizationsHelper
   def organization_form_fields(form)
     markaby do
       labeled_input 'Name', :for => :name do
-        form.text_field :name
+        text form.text_field :name
+        span.instructions "e.g. San Francisco Bike Kitchen"
       end
       labeled_input 'Key', :for => :key do
-        form.text_field :key
+        text form.text_field :key
+        span.instructions "e.g. sfbk, used for URL http://freehub.bikekitchen.org/sfbk"
       end
       labeled_input 'Timezone', :for => :timezone do
         time_zone_select :organization, :timezone
