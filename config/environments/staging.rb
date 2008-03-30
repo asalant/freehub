e128c002163bb3101c03566b31918dccb07ffe0a
Alon Salant <alon@salant.org>
2008-03-30 20:08:15 +0000

    smtp settings for engine yard

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,4 +16,5 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.smtp_settings = { :address => "smtp.ey02.engineyard.com", :port => 25 }
