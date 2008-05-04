64d7d84d5506aa19b7469c06eae9519e4e042b58
Alon Salant <alon@salant.org>
2008-05-04 23:06:48 +0000

    started working on arena's design.

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,20 +1,5 @@
 module OrganizationsHelper
 
-  def organization_header(organization)
-    markaby do
-      h2.organization_header! do
-        link_to organization.name, organization_key_path(:organization_key => organization.key)
-        text ' on '
-        link_to 'Freehub', root_path
-        end
-      div.organization_nav! do
-        link_to('Visits', sign_in_today_path(:organization_key => organization.key))
-        text ' | '
-        link_to('Reports', report_path(:action => 'index', :organization_key => organization.key))
-      end
-    end
-  end
-
   def organization_form_fields(form)
     markaby do
       labeled_input 'Name', :for => :name do
