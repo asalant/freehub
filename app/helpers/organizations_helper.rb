1d471e35b0254f78d5f0416384c1dfa49d178a56
Alon Salant <alon@salant.org>
2008-05-22 23:13:06 +0000

    Grey background. More form styling.

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -3,12 +3,12 @@ module OrganizationsHelper
   def organization_form_fields(form)
     markaby do
       labeled_input 'Name', :for => :name do
-        text form.text_field(:name)
-        span.instructions "e.g. San Francisco Bike Kitchen"
+        div { form.text_field(:name) }
+        p.instruct "For example 'San Francisco Bike Kitchen'"
       end
       labeled_input 'Key', :for => :key do
-        text form.text_field(:key)
-        span.instructions "e.g. sfbk, used for URL http://freehub.bikekitchen.org/sfbk"
+        div { form.text_field(:key) }
+        p.instruct "For example 'sfbk', used for URL http://freehub.bikekitchen.org/sfbk"
       end
       labeled_input 'Timezone', :for => :timezone do
         time_zone_select :organization, :timezone
