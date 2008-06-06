2427d01d049ec16ba334c756a4de9cf8258c129c
Alon Salant <alon@salant.org>
2008-06-06 05:34:34 +0000

    Simpler visit summary report.

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,42 +7,35 @@ class Report
 
   def summary
     date_condition = ""
-    date_condition += "and visits.datetime > #{date_from.to_db}" if date_from
-    date_condition += "and visits.datetime < #{date_to.to_db}" if date_to
+    date_condition += "and visits.datetime > '#{date_from.to_s(:db)}' " if date_from
+    date_condition += "and visits.datetime < '#{date_to.to_s(:db)}' " if date_to
     connection = ActiveRecord::Base.connection
-    all_result = connection.select_all(<<-END
-      select date(visits.datetime) as date, people.staff, count(*) as count
+    visits_result = connection.select_all(<<-END
+      select date(visits.datetime) as date, staff, member, volunteer, count(*) as count
         from visits
-        left join people on people.id = visits.person_id
         where 1 = 1
         #{date_condition}
-        group by date(visits.datetime), people.staff
+        group by date(visits.datetime), staff, member, volunteer
+        order by visits.datetime asc
       END
       )
-    members_result = connection.select_all(<<-END
-      select date(visits.datetime) as date, people.staff, count(*) as count
-        from visits
-        left join people on people.id = visits.person_id
-        left join services on services.person_id = people.id
-        where services.start_date < visits.datetime and services.end_date > visits.datetime and services.service_type_id = 'MEMBERSHIP'
-        #{date_condition}
-        group by date(visits.datetime), people.staff
-      END
-      )
-    result = {}
-    all_result.each do |row|
-      day = result[row['date']] || { :date => row['date'] }
-      day[:staff] = row['count'].to_i if row['staff'] == '0'
-      day[:patron] = row['count'].to_i if row['staff'] == '1'
-      result[day[:date]] = day
-    end
-    members_result.each do |row|
-      day = result[row['date']]
+    visit_days, day = [], {}
+    visits_result.each do |row|
+      date = ActiveRecord::ConnectionAdapters::Column.string_to_date(row['date'])
+      if day[:date] != date
+        day = { :date => date }
+        visit_days << day
+      end
       if row['staff'] == '1'
-        day[:patron] = day[:patron] - row['count'].to_i
+        day[:staff] = row['count'].to_i
+      elsif row['volunteer'] == '1'
+        day[:volunteer] = row['count'].to_i
+      elsif row['member'] == '1'
         day[:member] = row['count'].to_i
+      else
+        day[:patron]  = row['count'].to_i
       end
     end
-    result
+    { :visits => visit_days }
   end
 end
--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -2,7 +2,25 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   def test_summary
-    result = Report.new.summary
-    p result
+
+    Visit.create!(:person => people(:marty), :datetime => TzTime.local(2006,4,1,6,30))
+    2.times do
+      Visit.create!(:person => people(:mary), :datetime => TzTime.local(2006,4,1,6,30))
+    end
+    3.times do
+      Visit.create!(:person => people(:mary), :datetime => TzTime.local(2006,4,1,6,30), :volunteer => true)
+    end
+    4.times do
+      Visit.create!(:person => people(:daryl), :datetime => TzTime.local(2006,4,1,6,30))
+    end
+
+    result = Report.new(:date_from => Date.new(2006,4,1), :date_to => Date.new(2006,4,2)).summary
+    assert_not_nil result[:visits]
+    assert_equal 1, result[:visits].size
+    assert_equal Date.new(2006,4,1), result[:visits].first[:date]
+    assert_equal 1, result[:visits].first[:staff]
+    assert_equal 3, result[:visits].first[:volunteer]
+    assert_equal 2, result[:visits].first[:member]
+    assert_equal 4, result[:visits].first[:patron]
   end
 end
