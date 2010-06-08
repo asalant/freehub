cb98c16522097298c9cc86d8643a8533246811ce
Alon Salant <alon@salant.org>
2010-06-07 21:46:12 -0700

    remove unused helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,4 @@
 module UsersHelper
-  
-  def user_status
-    markaby do
-      div.user_status! do
-        if logged_in?
-          text "Logged in as #{link_to current_user.login, edit_user_path(current_user)}"
-          text "&nbsp;|&nbsp;"
-          link_to 'Log out', session_path, :method => :delete
-        else
-          link_to 'Log in', new_session_path
-        end
-      end
-    end
-  end
 
   def userstamp_labeled_values(model)
       [labeled_value('Created', "#{datetime_short(model.created_at)} by #{user_link(model.created_by)}"),
