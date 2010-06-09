7fab5b43ecd5729314ac367139e6437186cf6bfa
Alon Salant <alon@salant.org>
2010-06-08 22:04:27 -0700

    upgrade calendar_date_select to 1.16.1

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'hoptoad_notifier'
   config.gem 'googlecharts'
   config.gem 'tzinfo'
+  config.gem 'calendar_date_select'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
