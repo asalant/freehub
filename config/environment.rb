cd59af8bc32ad965c282e9d2221b89428d9c74a5
Alon Salant <alon@salant.org>
2010-06-08 22:23:30 -0700

    move newrelic_rpm to vendored gem

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem 'googlecharts'
   config.gem 'tzinfo'
   config.gem 'calendar_date_select'
+  config.gem "newrelic_rpm" 
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
