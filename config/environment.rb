e56989202943153c6887eecbb2284e9af6ceb46f
Alon Salant <alon@salant.org>
2010-06-08 22:39:27 -0700

    update annotate_models as a gem

--- config/environment.rb
+++ config/environment.rb
@@ -29,7 +29,8 @@ Rails::Initializer.run do |config|
   config.gem 'googlecharts'
   config.gem 'tzinfo'
   config.gem 'calendar_date_select'
-  config.gem "newrelic_rpm" 
+  config.gem "newrelic_rpm"
+  config.gem "annotate"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
