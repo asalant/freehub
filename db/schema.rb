30e0f3ba76559f6f974646bef136968bb0a80e3a
Alon Salant <alon@salant.org>
2010-06-11 15:45:59 -0700

    initial setup for tags

--- db/schema.rb
+++ db/schema.rb
@@ -94,6 +94,23 @@ ActiveRecord::Schema.define(:version => 20100626223127) do
   add_index "services", ["person_id"], :name => "fk_services_person"
   add_index "services", ["updated_by_id"], :name => "fk_services_updated_by"
 
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "taggable_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "login"
     t.string   "email"
