920b67890dc5c80e6fbc6aae0996e18fd48b6b54
Alon Salant <alon@salant.org>
2010-06-12 15:47:35 -0700

    more sanity tests for tagging

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -99,12 +99,26 @@ class PersonTest < ActiveSupport::TestCase
   context "A person Mary with tags" do
     setup do
       @person = people(:mary)
-      @person.tag_list = "member, helpful, artist"
-      @person.save!
+      @person.tag_list = "member, helpful, Artist"
+      @person.save! && @person.reload
     end
 
     should "save comma-separated tags" do
-      assert_equal %w[member helpful artist], @person.tag_list
+      assert_equal %w[member helpful Artist], @person.tag_list
+    end
+
+    should "add tag" do
+      @person.tag_list << 'other'
+      @person.save! && @person.reload
+
+      assert_equal %w[member helpful Artist other], @person.tag_list
+    end
+
+    should "not add same tag with different case" do
+      @person.tag_list << 'artist'
+      @person.save! && @person.reload
+
+      assert_equal %w[member helpful Artist], @person.tag_list
     end
   end
 end
