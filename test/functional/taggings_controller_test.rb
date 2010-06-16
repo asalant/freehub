365980f1fefb268058cffcd2463a3f84a784d296
Alon Salant <alon@salant.org>
2010-06-15 23:48:25 -0700

    prelim add existing tag

--- test/functional/taggings_controller_test.rb
+++ test/functional/taggings_controller_test.rb
@@ -4,25 +4,35 @@ class TaggingsControllerTest < ActionController::TestCase
   context 'A person with tags' do
     setup do
       login_as 'greeter'
-      
-      @person = people(:mary).tap do |p|
-        p.tag_list = 'one, two'
-        p.save!
-      end
     end
 
-    context 'when deleting a tag' do
+    context 'after deleting a tag' do
       setup do
-        delete :destroy, :organization_key => 'sfbk', :person_id => @person, :id => 'one'
-        @person.reload
+        delete :destroy, :organization_key => 'sfbk', :person_id => people(:mary), :id => 'mechanic'
+        people(:mary).reload
       end
 
       should_redirect_to 'show person' do
-        person_path(:organization_key => 'sfbk', :id => @person)
+        person_path(:organization_key => 'sfbk', :id => people(:mary))
       end
 
       should 'not have the deleted tag' do
-        assert_equal ['two'], @person.tag_list
+        assert_equal ['mom'], people(:mary).tag_list
+      end
+    end
+
+    context 'after adding a tag' do
+      setup do
+        put :create, :organization_key => 'sfbk', :person_id => people(:mary), :id => 'three'
+        people(:mary).reload
+      end
+
+      should_redirect_to 'show person' do
+        person_path(:organization_key => 'sfbk', :id => people(:mary))
+      end
+
+      should 'have the new tag' do
+        assert_equal ['mechanic', 'mom', 'three'], people(:mary).tag_list
       end
     end
   end
