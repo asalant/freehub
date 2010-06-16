f1420e8b07654d0b1fda699de7b6ca1f4c180245
Alon Salant <alon@salant.org>
2010-06-15 23:48:25 -0700

    prelim add existing tag

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -2,12 +2,20 @@ class TaggingsController < ApplicationController
   permit "admin or (manager of :organization)"
 
   def create
-
+    @person.tag_list << params[:id]
+    @person.save
+    redirect_to_person_path
   end
 
   def destroy
     @person.tag_list.delete params[:id]
     @person.save
+    redirect_to_person_path
+  end
+
+  private
+
+  def redirect_to_person_path
     redirect_to person_path(:organization_key => @organization.key, :id => @person)
   end
 end
