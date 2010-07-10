91e118c4cdf881d8ee14ecfd6fa29e361e541dce
Alon Salant <alon@salant.org>
2010-07-09 18:27:54 -0700

    switched from prototype helpers to jquery for tag add/delete

--- public/javascripts/tags.jquery.js
+++ public/javascripts/tags.jquery.js
@@ -5,10 +5,10 @@
 
         return $(this).each(function()
         {
-            var tags_control = this;
+            var tags_control = $(this);
 
-            $('.view', tags_control).show();
-            $('.edit', tags_control).hide();
+            $('.view', this).show();
+            $('.edit', this).hide();
 
             $('.view a.show_edit', this).live('click', function()
             {
@@ -22,6 +22,51 @@
                 $('.view', tags_control).show();
                 $('.edit', tags_control).hide();
                 return false;
+            });
+
+            $('.edit a.delete').live('click', function (event)
+            {
+                tags_control.trigger('delete', { url: $(this).attr('href') });
+                event.preventDefault();
+            });
+
+            $('select', this).live('change', function(event)
+            {
+                if ($(this).val() != '')
+                {
+                    tags_control.trigger('add', { url: $(this).parent('form').attr('action'), tag: $(this).val() });
+                }
+            });
+
+            tags_control.bind('delete', function(event, data)
+            {
+                tags_control.addClass('ajax_loading');
+                $.ajax({
+                    type: 'POST',
+                    url: data.url,
+                    data: { _method: 'DELETE', auth_token: window.AUTHENTICITY_TOKEN},
+                    success: function(data, status)
+                    {
+                        tags_control.replaceWith(data);
+                    }
+                });
+                return data;
+            });
+
+
+            tags_control.bind('add', function(event, data)
+            {
+                tags_control.addClass('ajax_loading');
+                $.ajax({
+                    type: 'POST',
+                    url: data.url,
+                    data: { _method: 'POST', auth_token: window.AUTHENTICITY_TOKEN, id: data.tag },
+                    success: function(data, status)
+                    {
+                        tags_control.replaceWith(data);
+                    }
+                });
+                return data;
             });
         });
     }
--- test/javascript/tags.jquery_spec.js
+++ test/javascript/tags.jquery_spec.js
@@ -3,7 +3,7 @@ require("../../public/javascripts/tags.jquery.js");
 
 Screw.Unit(function()
 {
-    describe("Tags plugin", function()
+    describe("Tags control", function()
     {
         before(function()
         {
@@ -11,7 +11,7 @@ Screw.Unit(function()
             $('.tags_control').tags_control();
         });
 
-        describe("view initialization", function()
+        describe("initializes view", function()
         {
             it("hides edit view", function()
             {
@@ -26,7 +26,7 @@ Screw.Unit(function()
             });
         });
 
-        describe("edit initialization", function()
+        describe("initializes edit", function()
         {
             it("shows view", function()
             {
@@ -37,6 +37,40 @@ Screw.Unit(function()
                 expect($(".tags_control .view").is(":visible")).to(be_true);
                 expect($(".tags_control .edit").is(":visible")).to(be_false);
             });
+
+        });
+
+        describe("deletes tag", function()
+        {
+           it("should dispatch delete event", function()
+           {
+               var event_data;
+               $(".tags_control").bind('delete', function(event, data)
+               {
+                   event_data = data;
+               });
+
+               $('.tags_control .edit a.delete:first').trigger('click');
+               expect(event_data).to_not(be_null);
+               expect(event_data.url).to(equal, '/sfbk/people/602306942/taggings/key%20holder');
+           });
+        });
+
+        describe("adds tag", function()
+        {
+           it("should dispatch add event", function()
+           {
+               var event_data;
+               $(".tags_control").bind('add', function(event, data)
+               {
+                   event_data = data;
+               });
+
+               $('.tags_control select').val('two').trigger('change');
+               expect(event_data).to_not(be_null);
+               expect(event_data.url).to(equal, '/sfbk/people/602306942/taggings');
+               expect(event_data.tag).to(equal, 'two');
+           });
 
         });
     });
