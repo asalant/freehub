fcc6089561f04fb18cb2862bb21b2f1a57c07b31
Alon Salant <alon@salant.org>
2026-01-20 16:44:52 -0800

    Resolve mysql 8 SQL group by error

--- app/models/visits_summary.rb
+++ app/models/visits_summary.rb
@@ -18,18 +18,18 @@ class VisitsSummary
     date_condition += "and visits.arrived_at > '#{criteria[:from].to_date.to_time.utc.to_s(:db)}' " if criteria[:from]
     date_condition += "and visits.arrived_at < '#{criteria[:to].to_date.to_time.utc.to_s(:db)}' " if criteria[:to]
     visits_result = ActiveRecord::Base.connection.select_all(<<-END
-      select date(convert_tz(visits.arrived_at,'+00:00','#{Time.zone.formatted_offset}')) as date, visits.staff, visits.member, visits.volunteer, count(*) as count
+      select date(convert_tz(visits.arrived_at,'+00:00','#{Time.zone.formatted_offset}')) as arrived_at_date, visits.staff, visits.member, visits.volunteer, count(*) as count
         from visits
         left join people on visits.person_id = people.id
         where people.organization_id = #{criteria[:organization_id]}
         #{date_condition}
-        group by date(visits.arrived_at), visits.staff, visits.member, visits.volunteer
-        order by visits.arrived_at asc
+        group by arrived_at_date, visits.staff, visits.member, visits.volunteer
+        order by arrived_at_date asc
       END
       )
     visit_days, day = [], nil
     visits_result.each do |row|
-      date = ActiveRecord::ConnectionAdapters::Column.string_to_date(row['date'])
+      date = ActiveRecord::ConnectionAdapters::Column.string_to_date(row['arrived_at_date'])
       if day.nil? || day.date != date
         day = VisitsDay.new(date)
         visit_days << day
