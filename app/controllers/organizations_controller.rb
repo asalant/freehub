830c8499ff5f5c08d56383c7c3ea83e31175c924
Alon Salant <alon@salant.org>
2026-01-26 15:33:12 -0800

    Cache homepage daily for non-logged-in users

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,8 @@
 class OrganizationsController < ApplicationController
 
+  caches_action :index, :expires_in => 24.hours,
+                :if => Proc.new { |c| !c.send(:logged_in?) }
+
   skip_before_filter :login_required, :only => [:index, :show, :new, :create]
   before_filter :assign_id_param, :resolve_organization_by_id, :except => [ :index, :new, :create ] 
 
